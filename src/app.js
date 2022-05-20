6053fae14ffe61435cfd1a3cd9de778f8537f12a
TaTi <allaoleksyn@gmail.com>
2022-05-20 18:59:10 +0300

    test

--- src/app.js
+++ src/app.js
@@ -60,3 +60,14 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = Math.round((14 * 9) / 5 + 32);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = fahrenheitTemperature;
+}
+let faherheitLink = document.querySelector("#fahrenheit-link");
+faherheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+search("Uzhgorod");
